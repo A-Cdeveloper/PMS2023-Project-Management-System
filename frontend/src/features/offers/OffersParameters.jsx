655e9171afd684b6fe0c4ef082edd5b3761a9a5b
A-Cdeveloper <aleksandar@e-vlasotince.info>
2023-10-29 14:35:58 +0100

    add service_description to allServices array

--- frontend/src/features/offers/OffersParameters.jsx
+++ frontend/src/features/offers/OffersParameters.jsx
@@ -44,10 +44,13 @@ export const offerAllServices = () => {
 export const allServices = () => {
   const { services } = useServices();
 
+  console.log(services);
+
   const servicesFormated = services.map((service) => {
     return {
       service_id: service.service_id,
       service_name: service.service_name,
+      service_description: service.service_description,
       service_price_hour: service.service_price_hour,
       service_price_total: service.service_price_total,
     };
