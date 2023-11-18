ace69c493591a5383381e284a8127f8870ac5daa
A-Cdeveloper <aleksandar@e-vlasotince.info>
2023-11-18 02:08:02 +0100

    initial project page if no projects

--- frontend/src/features/projects/ProjectsTableOperations.jsx
+++ frontend/src/features/projects/ProjectsTableOperations.jsx
@@ -1,3 +1,4 @@
+import { useQueryClient } from "@tanstack/react-query";
 import TableOperations from "../../ui/Data/TableOperations";
 import SortBy from "../../ui/SortBy";
 import FilterText from "../../ui/FilterText";
@@ -8,50 +9,57 @@ import AddEditProject from "./AddEditProject";
 import { projectStatus } from "./ProjectParameters";
 
 function ClientsTableOperations() {
+  const queryClient = useQueryClient();
+  const projects = queryClient.getQueryData(["projects"]) || [];
+
   return (
     <TableOperations>
-      <FilterText placeholder="Search projects..." />
-      <Filter
-        filterField="status"
-        options={projectStatus.map((status) => {
-          return { value: status.value, label: status.label };
-        })}
-      />
+      {projects.length !== 0 && (
+        <>
+          <FilterText placeholder="Search projects..." />
+          <Filter
+            filterField="status"
+            options={projectStatus.map((status) => {
+              return { value: status.value, label: status.label };
+            })}
+          />
 
-      <SortBy
-        options={[
-          { value: "project_name-asc", label: "Sort by Name (asc)" },
-          { value: "project_name-desc", label: "Sort by Name (desc)" },
-          { value: "client_name-asc", label: "Sort by Client (asc)" },
-          { value: "client_name-desc", label: "Sort by Client (desc)" },
-          {
-            value: "task_per_project-asc",
-            label: "Sort by Tasks num (asc)",
-          },
-          {
-            value: "task_per_project-desc",
-            label: "Sort by Tasks num (desc)",
-          },
+          <SortBy
+            options={[
+              { value: "project_name-asc", label: "Sort by Name (asc)" },
+              { value: "project_name-desc", label: "Sort by Name (desc)" },
+              { value: "client_name-asc", label: "Sort by Client (asc)" },
+              { value: "client_name-desc", label: "Sort by Client (desc)" },
+              {
+                value: "task_per_project-asc",
+                label: "Sort by Tasks num (asc)",
+              },
+              {
+                value: "task_per_project-desc",
+                label: "Sort by Tasks num (desc)",
+              },
 
-          {
-            value: "project_start_date-asc",
-            label: "Sort by Start date (earlier first)",
-          },
-          {
-            value: "project_start_date-desc",
-            label: "Sort by Start date (recent first)",
-          },
-          {
-            value: "project_end_date-asc",
-            label: "Sort by End date (earlier first)",
-          },
-          {
-            value: "project_end_date-desc",
-            label: "Sort by End date (recent first)",
-          },
-        ]}
-        defaultOptionIndex={0}
-      />
+              {
+                value: "project_start_date-asc",
+                label: "Sort by Start date (earlier first)",
+              },
+              {
+                value: "project_start_date-desc",
+                label: "Sort by Start date (recent first)",
+              },
+              {
+                value: "project_end_date-asc",
+                label: "Sort by End date (earlier first)",
+              },
+              {
+                value: "project_end_date-desc",
+                label: "Sort by End date (recent first)",
+              },
+            ]}
+            defaultOptionIndex={0}
+          />
+        </>
+      )}
       <NewRecord record="project">
         <AddEditProject />
       </NewRecord>
