b96cf867789ff5a72af54a6a6b66c302f2daecb9
A-Cdeveloper <aleksandar@e-vlasotince.info>
2023-11-19 02:17:48 +0100

    clear

--- frontend/src/features/clients/ClientsTableOperations.jsx
+++ frontend/src/features/clients/ClientsTableOperations.jsx
@@ -3,16 +3,10 @@ import SortBy from "../../ui/SortBy";
 import FilterText from "../../ui/FilterText";
 import NewRecord from "../../ui/Buttons/NewRecord";
 import AddEditClient from "./AddEditClient";
-// import { useQueryClient } from "@tanstack/react-query";
 
 function ClientsTableOperations() {
-  // const queryClient = useQueryClient();
-  // const clients = queryClient.getQueryData(["clients"]) || [];
-
   return (
     <TableOperations>
-      {/* {clients.length !== 0 && (
-        <> */}
       <FilterText placeholder="Search clients..." />
       <SortBy
         options={[
@@ -29,8 +23,7 @@ function ClientsTableOperations() {
         ]}
         defaultOptionIndex={0}
       />
-      {/* </>
-      )} */}
+
       <NewRecord record="client">
         <AddEditClient />
       </NewRecord>
