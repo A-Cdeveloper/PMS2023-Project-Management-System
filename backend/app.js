90f591c8ed548ff2ddeaaa60b985bf03ffae46c5
A-Cdeveloper <aleksandar@e-vlasotince.info>
2023-12-13 00:19:14 +0100

    enable cors

--- backend/app.js
+++ backend/app.js
@@ -17,9 +17,16 @@ app.use(
 app.use(express.static('public'))
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   next()
 })
 
