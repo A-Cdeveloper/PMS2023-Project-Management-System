b4b0deb4cc2581b7d35aae446094e5273d1c7e08
A-Cdeveloper <aleksandar@e-vlasotince.info>
2024-01-20 15:04:29 +0100

    add cors package

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const dotenv = require('dotenv')
+const cors = require('cors')
 dotenv.config()
 const PORT = process.env.PORT
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(
   fileUpload({
