399216ab12974d8e70edca2a2cb23eb4ded1323c
A-Cdeveloper <aleksandar@e-vlasotince.info>
2024-01-27 23:32:33 +0100

    optimise with milion.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
+import million from "million/compiler";
 import { visualizer } from "rollup-plugin-visualizer";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer()],
+  plugins: [
+    react(),
+    visualizer(),
+    million.vite({
+      auto: {
+        threshold: 0.05,
+        skip: ["useBadHook", /badVariable/g],
+      },
+    }),
+  ],
 });
